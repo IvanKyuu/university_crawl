28fbe7a668793d0920966a2886a7acee7ca492de
ivanshan_8170 <ivan.shan@huawei.com>
2024-05-07 15:31:54 -0400

    add usnews

--- university_info_generator/fetcher/ranking_fetcher/ranking_fetcher.py
+++ university_info_generator/fetcher/ranking_fetcher/ranking_fetcher.py
@@ -5,9 +5,9 @@ import numpy as np
 from selenium.common.exceptions import TimeoutException
 
 from configs.config import PROJECT_HOME
-# from us_news_ranking_2024 import USNewsRankingCrawl
 
 __all__ = ["RankingFetcher"]
+
 
 class RankingFetcher:
     repo_path = f"{PROJECT_HOME}/university_info_generator/fetcher/ranking_fetcher/"
@@ -16,7 +16,7 @@ class RankingFetcher:
         self.ranking_arwu_rank_2023_df = pd.read_csv(f"{self.repo_path}/ranking_data/arwu_ranking_2023.csv")
         self.ranking_qs_news_2024_df = pd.read_csv(f"{self.repo_path}/ranking_data/qs_ranking_2024.csv")
         self.ranking_times_rank_2024_df = pd.read_csv(f"{self.repo_path}/ranking_data/times_ranking_2024.csv")
-        # self.usnews_crawl = USNewsRankingCrawl()
+        self.ranking_us_news_2023_df = pd.read_csv(f"{self.repo_path}/ranking_data/us_news_ranking_2023.csv")
 
     def get_ranking(
         self,
@@ -25,7 +25,12 @@ class RankingFetcher:
             "ranking_qs_news_2024", "ranking_us_news_2023", "ranking_times_rank_2024", "ranking_arwu_rank_2023"
         ],
     ) -> Optional[Dict[str, str]]:
-        if ranking_type in ("ranking_qs_news_2024", "ranking_times_rank_2024", "ranking_arwu_rank_2023"):
+        if ranking_type in (
+            "ranking_qs_news_2024",
+            "ranking_times_rank_2024",
+            "ranking_arwu_rank_2023",
+            "ranking_us_news_2023",
+        ):
             df_name = ranking_type + "_df"
             dataframe = getattr(self, df_name)
             filtered_df = dataframe[dataframe["university_name"].str.contains(university_name, case=False, na=False)]
@@ -33,19 +38,8 @@ class RankingFetcher:
             if len(dicts) != 1:
                 return None
             return dicts[0]
-        # TODO: ranking_qs_news_2024
-        print( NotImplementedError("ranking_qs_news_2024"))
-        return None
-        # try:
-        #     self.usnews_crawl.get_page_content_by_name(name=university_name)
-        #     print(self.usnews_crawl.ranking_info)
-        #     return self.usnews_crawl.ranking_info[university_name]
-        # except TimeoutException:
-        #     return None
-        # finally:
-        #     self.usnews_crawl.close()
 
 
 if __name__ == "__main__":
     ran_fe = RankingFetcher()
-    print(ran_fe.get_ranking("Waterloo", "ranking_us_news_2023"))
+    print(ran_fe.get_ranking("University of Toronto", "ranking_us_news_2023"))
